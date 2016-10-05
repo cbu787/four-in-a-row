65a03d2f459d9ef5ef241eacfa488ed7e674ec40
CALLUM BURNS <CBU787@GMAIL.COM>
2016-10-04 21:04:02 -0400

    Added Javascript to change colour on click.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,16 @@
-$(document).on("ready", function() {
-  var winningCombos = []
+$(document).on('ready', function () {
+  // var winningCombos = []
+  var turn = 0;
 
-})
+  $('td').on('click', function () {
+
+    if (turn % 2 === 0) {
+      $(this).addClass('yellow').css('background-color', 'yellow');
+    } else {
+      $(this).addClass('red').css('background-color', 'red');
+    }
+
+    $(this).off('click');
+    turn++;
+  });
+});
